ddd80fa93620a316aae09f328b8d56aa6b6e8289
newfaji <158270331+newfaji@users.noreply.github.com>
2025-05-26 17:59:47 +0900

    .

---
+++ animal/js/main.js
@@ -0,0 +1,28 @@
+//console.log('연결')
+$(document).ready(function(){
+    //console.log('나와?????')
+    const visual_swiper = new Swiper('.visual .swiper', { /* 팝업을 감싼는 요소의 class명 */
+        autoplay: {  /* 팝업 자동 실행 */
+             delay: 5000,
+             disableOnInteraction: true,
+        },
+        //effect: "fade", /* fade 효과 */
+        loop: true,  /* 마지막 팝업에서 첫번째 팝업으로 자연스럽게 넘기기 */
+        navigation: {  /* 이전, 다음 버튼 */
+            nextEl: '.visual .btn_wrap button.btn_next',  /* 다음 버튼의 클래스명 */
+            prevEl: '.visual .btn_wrap button.btn_prev',  
+        },
+    });
+    $('.visual .btn_wrap button.btn_stop').on('click', function(){
+        //console.log('일시정지 버튼 클릭')
+        visual_swiper.autoplay.stop();  /* 일시정지 기능 */
+        $(this).hide() //정지버튼 자신은 숨김
+        $('.visual .btn_wrap button.btn_play').show() //재생 나타남
+    })
+    $('.visual .btn_wrap button.btn_play').on('click', function(){
+        //console.log('재생 버튼 클릭')
+        visual_swiper.autoplay.start();  /* 재생 기능 */
+        $(this).hide() //재생버튼 자신은 숨김
+        $('.visual .btn_wrap button.btn_stop').show() //정지버튼이 나타남
+    })
+})
